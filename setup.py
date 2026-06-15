1a8974b72fbbaa52bc096bade1d8ea344828c8f2
Nandu Ahmed <nandu.ahmed@gmail.com>
2026-06-16 00:52:17 +0530

    chore: bump version to 0.3.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="trend-rider-lib",
-    version="0.3.2",
+    version="0.3.3",
     description="Stock trend analysis and trading system with state machine architecture",
     author="Trend Rider",
     packages=find_packages(),
