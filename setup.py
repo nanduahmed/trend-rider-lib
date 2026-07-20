1f8265bcbf97732924cd6b9b1cd18106a34ba451
Nandu Ahmed <nandu.ahmed@gmail.com>
2026-07-20 16:16:12 +0530

    fix: add UTF-8 encoding to requirements file read in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ Setup configuration for Trend Rider library.
 """
 from setuptools import setup, find_packages
 
-with open("requirements.txt") as f:
+with open("requirements.txt", encoding="utf-8") as f:
     requirements = f.read().splitlines()
 
 setup(
